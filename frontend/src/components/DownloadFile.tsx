028076c8cdf82b22dd587ce54e8ccb3916372bbf
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-21 15:48:40 +0100

    download page link component

---
+++ frontend/src/components/DownloadFile.tsx
@@ -0,0 +1,18 @@
+const DownloadFile = ({downloadPageLink}) => {
+    return (
+        <div className="p-1">
+            <h1 className="my-2 text-lg font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, quaerat?</h1>
+            <div className="flex space-x-3">
+                <span className="break-all">{downloadPageLink}</span>
+                <img
+                    src="copy.png"
+                    alt="copy file"
+                    className="w-8 h-8 object-contain cursor-pointer"
+                    onClick={() => navigator.clipboard.writeText(downloadPageLink)}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default DownloadFile;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,16 +2,17 @@ import {useState} from 'react';
 import axios from 'axios';
 import DropZone from "@components/DropZone";
 import RenderFile from "@components/RenderFile";
+import DownloadFile from "@components/DownloadFile";
 
 export default function Home() {
     const [file, setFile] = useState(null);
     const [id, setId] = useState(null);
     const [downloadPageLink, setDownloadPageLink] = useState(null);
-    const [uploadState, setUploadState] = useState<"Uploading" | "Upload Failed" | "Uploaded">(null)
+    const [uploadState, setUploadState] = useState<"Uploading" | "Upload Failed" | "Uploaded" | "Upload">("Upload")
 
     const handleUpload = async () => {
         if (uploadState === "Uploading") return;
-
+        setUploadState("Uploading");
         const formData = new FormData();
         formData.append("myFile", file);
         try {
@@ -36,7 +37,8 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center">
             <h1 className="my-4 text-3xl font-medium">Got a File? Share it!</h1>
             <div className="w-96 flex flex-col items-center bg-gray-800 shadow-xl rounded-xl justify-center">
-                <DropZone setFile={setFile}/>
+
+                {!downloadPageLink && <DropZone setFile={setFile}/>}
 
                 {file && (
                     <RenderFile file={{
@@ -46,11 +48,25 @@ export default function Home() {
                     }}
                     />
                 )}
-                {/*upload button*/}
-                <button
-                    className="p-2 my-5 bg-gray-900 rounded-md w-44 focus:outline-none"
-                    onClick={handleUpload}
-                >Upload</button>
+
+                {!downloadPageLink && file && (
+                        <button
+                            className="p-2 my-5 bg-gray-900 rounded-md w-44 focus:outline-none"
+                            onClick={handleUpload}
+                        >
+                            {uploadState}
+                        </button>)
+                }
+
+                {downloadPageLink && (
+                    <div className="p-2 text-center">
+                        <DownloadFile downloadPageLink={downloadPageLink}/>
+                        {/*Email Form*/}
+                        <button className="p-2 my-5 bg-gray-900 rounded-md w-44 focus:outline-none">
+                            Upload New File
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
