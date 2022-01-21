5992db1aceed87881a7a2398eb934696dcba99eb
krutulkamil <79461468+krutulkamil@users.noreply.github.com>
2022-01-21 21:35:13 +0100

    send email frontend

--- frontend/src/components/EmailForm.tsx
+++ frontend/src/components/EmailForm.tsx
@@ -1,4 +1,5 @@
 import {FunctionComponent, useState} from "react";
+import axios from "axios";
 
 const EmailForm: FunctionComponent<{
     id: string
@@ -7,10 +8,30 @@ const EmailForm: FunctionComponent<{
     const [emailTo, setEmailTo] = useState('');
     const [message, setMessage] = useState(null);
 
+    const handleEmail = async (e) => {
+        e.preventDefault();
+
+        try {
+            const {data} = await axios({
+                method: "POST",
+                url: "api/files/email",
+                data: {
+                    id,
+                    emailFrom,
+                    emailTo
+                }
+            });
+
+            setMessage(data.message);
+        } catch (error) {
+            setMessage(error.data.response.message);
+        }
+    };
+
     return (
         <div className="form">
             <h3>You can also send a file through mail</h3>
-            <form className="form">
+            <form className="form" onSubmit={handleEmail}>
                 <input
                     className="form-input"
                     type="email"
@@ -34,6 +55,9 @@ const EmailForm: FunctionComponent<{
                     Send Email
                 </button>
             </form>
+            {message && (
+                <p className="font-medium text-red-500">{message}</p>
+            )}
         </div>
     );
 };
